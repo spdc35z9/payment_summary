b3c5e3134ebbfb80b8637021592143b3955c7d4f
spdc35z9 <spdc35z9@gmail.com>
2019-10-31 01:01:00 +0900

    Update paymentSummary.py

--- source/paymentSummary.py
+++ source/paymentSummary.py
@@ -1,19 +1,33 @@
 import sys
 import json
 
-def main():
+def paymentSummary():
 
+    #jsonファイルディレクトリ
     input_json = 'C:\\Users\\spdc3\\OneDrive\\デスクトップ\\payment_summary\\json\\account.json'
 
     with open (input_json) as f:
         json_dict = json.load(f)
 
+    #各サイトのスクレイピング
     for i,d in enumerate(json_dict):
-        url = json_dict[i]['url']
-        id = json_dict[i]['id']
-        password = json_dict[i]['pass']
-        print(url)
-        print(id)
-        print(password)
+        domain = json_dict[i]['domain']
+        #水道ネット
+        if domain == 'suidonet':
+            print()
+        #マイガス
+        elif domain == 'suidonet':
+            print()
+        #スマ電
+        elif domain == 'suidonet':
+            print()
+        else:
+            print()
+        # url = json_dict[i]['url']
+        # id = json_dict[i]['id']
+        # password = json_dict[i]['pass']
+        # print(url)
+        # print(id)
+        # print(password)
 
-main()
+paymentSummary()
