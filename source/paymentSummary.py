fb059716a8c240ce15c6ce135a249be77fb3b230
spdc35z9 <spdc35z9@gmail.com>
2019-11-02 00:37:03 +0900

    Update paymentSummary.py

--- source/paymentSummary.py
+++ source/paymentSummary.py
@@ -1,10 +1,11 @@
-import sys
+# coding: shift_jis
 import json
+from domains.getSuidoNet import getSuidoNet
 
 def paymentSummary():
 
     #jsonファイルディレクトリ
-    input_json = 'C:\\Users\\spdc3\\OneDrive\\デスクトップ\\payment_summary\\json\\account.json'
+    input_json = 'C:\\Users\\spdc3\\OneDrive\\デスクトップ\payment_summary\\json\\account.json'
 
     with open (input_json) as f:
         json_dict = json.load(f)
@@ -12,22 +13,15 @@ def paymentSummary():
     #各サイトのスクレイピング
     for i,d in enumerate(json_dict):
         domain = json_dict[i]['domain']
+        url = json_dict[i]['url']
+        id = json_dict[i]['id']
+        password = json_dict[i]['pass']
+
         #水道ネット
         if domain == 'suidonet':
-            print()
-        #マイガス
-        elif domain == 'suidonet':
-            print()
-        #スマ電
-        elif domain == 'suidonet':
-            print()
-        else:
-            print()
-        # url = json_dict[i]['url']
-        # id = json_dict[i]['id']
-        # password = json_dict[i]['pass']
-        # print(url)
-        # print(id)
-        # print(password)
+            target_date,receipt_amount = getSuidoNet(url,id,password)
+            print('対象年月:{}'.format(target_date))
+            print('領収金額:{}'.format(receipt_amount))
+
 
 paymentSummary()
